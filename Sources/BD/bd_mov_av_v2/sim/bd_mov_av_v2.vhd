b63b721f0e9ea1f290d116df4f702b3171af6c8f
putoale <61283317+putoale@users.noreply.github.com>
2020-05-26 14:23:31 +0200

    -

--- Sources/BD/bd_mov_av_v2/sim/bd_mov_av_v2.vhd
+++ Sources/BD/bd_mov_av_v2/sim/bd_mov_av_v2.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
---Date        : Mon May 25 15:43:47 2020
+--Date        : Tue May 26 14:15:39 2020
 --Host        : DESKTOP-O39JAIK running 64-bit major release  (build 9200)
 --Command     : generate_target bd_mov_av_v2.bd
 --Design      : bd_mov_av_v2
@@ -122,23 +122,6 @@ architecture STRUCTURE of bd_mov_av_v2 is
     edge_detected : out STD_LOGIC
   );
   end component bd_mov_av_v2_edge_detector_1_0;
-  component bd_mov_av_v2_volume_controller_0_0 is
-  port (
-    aclk : in STD_LOGIC;
-    aresetn : in STD_LOGIC;
-    volume_up : in STD_LOGIC;
-    volume_down : in STD_LOGIC;
-    s_axis_tvalid : in STD_LOGIC;
-    s_axis_tready : out STD_LOGIC;
-    s_axis_tdata : in STD_LOGIC_VECTOR ( 15 downto 0 );
-    s_axis_tlast : in STD_LOGIC;
-    m_axis_tvalid : out STD_LOGIC;
-    m_axis_tready : in STD_LOGIC;
-    m_axis_tdata : out STD_LOGIC_VECTOR ( 15 downto 0 );
-    m_axis_tlast : out STD_LOGIC;
-    volume_level : out STD_LOGIC_VECTOR ( 15 downto 0 )
-  );
-  end component bd_mov_av_v2_volume_controller_0_0;
   component bd_mov_av_v2_moving_average_v2_0_0 is
   port (
     clk : in STD_LOGIC;
@@ -170,6 +153,23 @@ architecture STRUCTURE of bd_mov_av_v2 is
     m_mute_tlast : out STD_LOGIC
   );
   end component bd_mov_av_v2_mute_v1_0_0;
+  component bd_mov_av_v2_volume_controller_0_0 is
+  port (
+    aclk : in STD_LOGIC;
+    aresetn : in STD_LOGIC;
+    volume_up : in STD_LOGIC;
+    volume_down : in STD_LOGIC;
+    s_axis_tvalid : in STD_LOGIC;
+    s_axis_tready : out STD_LOGIC;
+    s_axis_tdata : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    s_axis_tlast : in STD_LOGIC;
+    m_axis_tvalid : out STD_LOGIC;
+    m_axis_tready : in STD_LOGIC;
+    m_axis_tdata : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    m_axis_tlast : out STD_LOGIC;
+    volume_level : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+  end component bd_mov_av_v2_volume_controller_0_0;
   signal AXI4Stream_UART_0_M00_AXIS_RX_TDATA : STD_LOGIC_VECTOR ( 7 downto 0 );
   signal AXI4Stream_UART_0_M00_AXIS_RX_TREADY : STD_LOGIC;
   signal AXI4Stream_UART_0_M00_AXIS_RX_TVALID : STD_LOGIC;
